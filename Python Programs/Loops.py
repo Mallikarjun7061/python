23b9079c05949cd3ef2623fe1e912e1bc1c1bcc7
Mallikarjun7061 <99545745+Mallikarjun7061@users.noreply.github.com>
2022-06-17 17:06:13 +0530

    Add files via upload

---
+++ Python Programs/Loops.py
@@ -0,0 +1,59 @@
+                #FOR LOOP
+ 
+# n = "Mallikarjun"
+# for i in n:
+#     print(i)
+# for i in (7,43,6,6,"Mallikarjun"):    # List of Sequence Values
+#     print(i)
+# for i in range(1, 11):
+#     print(i, end=" ")
+# for i in range(20, 5, -1):
+#     print(i, end=" ")
+# for i in range(1, 21):
+#     if i%5 != 0:
+#         print(i)
+
+    
+
+         #WHILE LOOP
+         
+# i = 0
+# while i < 5:
+#     print("Hello")
+#     i += 1
+# i = 5
+# while i>=1:
+#     print("Hello")
+#     i -= 1
+# i = 1
+# while i <= 5:
+#     print("Mallu", i)
+#     i += 1
+
+
+
+      #IF ELSE
+
+
+# a = 22
+# if a % 2 == 0:
+#     print("Even")
+#     if a > 10:
+#         print("Greater Than 10")
+#     else:
+#         print("Less Than 10")
+# else:
+#     print("Odd")
+# print("Bye")
+
+      
+      #FOR ELSE
+
+nums = [12, 30, 26, 71, 34, 54]
+
+for num in nums:
+    if num % 5 == 0:
+        print(num)
+        break
+else:
+    print("Not Found")
