9c810b7c69ea8e4fc4f0435b9986d67f71f7309b
Mallikarjun7061 <99545745+Mallikarjun7061@users.noreply.github.com>
2022-06-17 15:33:05 +0530

    Add files via upload

---
+++ Python Programs/Bitwise.py
@@ -0,0 +1,27 @@
+# 1. Complement ( ~ -->  Complement)
+# 2. And ( & ) , Or ( | )  , XOR ( ^ )
+# 3. Left Shift ( << ) Right Shift ( >> )
+print(~17)
+print(~25)
+print(~41)
+print(~13)
+print("\n\n")
+
+
+print(12 & 13)
+
+print(12 | 13)
+
+print(28 | 15)
+
+print(25 | 30)
+
+print("\n\n")
+
+
+print(25 ^ 30)
+print(28 ^ 15)
+print("\n\n")
+
+print(10 << 2)
+print(10 >> 2)
