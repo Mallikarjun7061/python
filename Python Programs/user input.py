2d1698eda580cb7cf01176cf9ee9eddda8e3ba65
Mallikarjun7061 <99545745+Mallikarjun7061@users.noreply.github.com>
2022-06-20 11:59:37 +0530

    Add files via upload

---
+++ Python Programs/user input.py
@@ -0,0 +1,18 @@
+# x =input("enter the value")
+# print (type(x))
+# a=int(x)
+# y =input("enter the value")
+# b =int(y)
+# z =a+b
+# print (z)
+
+
+# x=int(input("enter the number"))
+# y=int(input("enterr the number"))
+# z=x+y
+# print(z)
+
+
+
+result=eval(input('enter an expr'))
+print(result)
