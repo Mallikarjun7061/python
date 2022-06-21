673a083e2f53c4eb6ff8ed82655e48fd7041c7b6
Mallikarjun7061 <99545745+Mallikarjun7061@users.noreply.github.com>
2022-06-21 11:41:19 +0530

    Add files via upload

---
+++ Python Programs/Swap2Num.py
@@ -0,0 +1,37 @@
+class TopTen:
+
+    def __init__(self):
+        self.num = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self.num <= 10:
+            val = self.num
+            self.num += 1
+
+            return val
+        else:
+            raise StopIteration
+
+
+values = TopTen()
+
+print(next(values))
+
+for i in values:
+    print(i)
+
+
+# num =47
+
+# for i in range(2,num):
+#     if num % i ==0:
+#         print("Not prime")
+#         break
+
+
+# else:
+#     print("prime")
